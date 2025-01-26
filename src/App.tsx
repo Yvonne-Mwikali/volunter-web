b5236d6fe4356279170667bbf6e65db4893be6ed
yvvii <yvonnemwikali162@gmail.com>
2025-01-26 13:42:21 +0300

    added a not found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import "./App.css";
 import { RootLayout } from "./pages/layout";
 import { MainHomePages } from "./pages/main";
+import NotFound from "./pages/not-found";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
+      errorElement: <NotFound />,
       children: [
         {
           path: "/",
